48dbe79b77c31af834b99b5684edabbed1e4e6af
ivanbuhov <ivanbuhov@gmail.com>
2013-11-21 16:46:44 +0200

    Everything is working correctly.

--- Family/Family.Services/Controllers/PeopleController.cs
+++ Family/Family.Services/Controllers/PeopleController.cs
@@ -155,7 +155,7 @@ namespace Family.Services.Controllers
             // If the person has any parents and has spouse or children he can't be deleted
             if ((person.FirstParentId != null || person.SecondParentId != null) && (person.SpouseId != null || personChildren.Count() > 0))
             {
-                throw new FamilyException("The person can't be deleted because the pedigree must be divided in two parts. To delete the person, you must first delete the people in one of the parts.");
+                throw new FamilyException("The person can't be deleted because the pedigree will be divided in two parts. To delete the person, you must first delete the people in one of the parts.");
             }
 
             if (person.Spouse != null)
